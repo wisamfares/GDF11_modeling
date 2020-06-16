77eca668a04af2631c43066e95ec2db54fe36f0f
Wisam Fares <wf8ga@virignia.edu>
2020-06-16 18:10:41 -0400

    Add description erf_Tijk.m

--- erf_Tijk.m
+++ erf_Tijk.m
@@ -1,6 +1,7 @@
 function [E_Tijk] = erf_Tijk(Ai_0, Lj, Bk_0,K_ij, gamma_j, K_ijk, T_ijk)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%Error Function as described in Elowtiz paper, Eq. 24
+%   Note, Eq. 24 includes a minus sign instead of a plus sign, this is
+%   most likely a typo, given the derivation in Eqs. 21-23. 
 E_Tijk = 0; 
 for i = 1:2
     for j = 1:2 
